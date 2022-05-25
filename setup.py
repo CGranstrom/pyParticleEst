815596587fccf1bfc85f77743e48d59cd3d9beca
CGranstrom <45301498+CGranstrom@users.noreply.github.com>
2022-05-25 11:01:00 -0700

    make numpy accessible during installation (#1)

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ ext = '.pyx' if USE_CYTHON else '.c'
 
 if (not on_rtd):
     import numpy
-    extensions = [Extension("pyparticleest.utils.ckalman", ["pyparticleest/utils/ckalman" + ext]),
+    extensions = [Extension("pyparticleest.utils.ckalman", ["pyparticleest/utils/ckalman" + ext],
+                            include_dirs=[numpy.get_include()]),
                   Extension("pyparticleest.utils.cmlnlg_compute", ["pyparticleest/utils/cmlnlg_compute" + ext],
                             include_dirs=[numpy.get_include()])]
 else:
